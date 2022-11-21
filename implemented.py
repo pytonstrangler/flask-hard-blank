593f33bff93724e1a3b1e7e9a37221b6564ed908
artem <artyomahma@gmail.com>
2022-11-21 08:29:12 +0500

    q

--- implemented.py
+++ implemented.py
@@ -1,7 +1,18 @@
-# файл для создания DAO и сервисов чтобы импортировать их везде
+from dao.director import DirectorDAO
+from dao.genre import GenreDAO
+from dao.movie import MovieDAO
 
-# book_dao = BookDAO(db.session)
-# book_service = BookService(dao=book_dao)
-#
-# review_dao = ReviewDAO(db.session)
-# review_service = ReviewService(dao=review_dao)
+from service.director import DirectorService
+from service.genre import GenreService
+from service.movie import MovieService
+
+from setup_db import db
+
+director_dao = DirectorDAO(session=db.session)
+genre_dao = GenreDAO(session=db.session)
+movie_dao = MovieDAO(session=db.session)
+
+director_service = DirectorService(dao=director_dao)
+genre_service = GenreService(dao=genre_dao)
+movie_service = MovieService(dao=movie_dao)
+
---
+++ views/movies.py
@@ -0,0 +1,48 @@
+from flask import request
+from flask_restx import Resource, Namespace
+
+from dao.model.movie import MovieSchema
+from implemented import movie_service
+
+movie_ns = Namespace('movies')
+
+@movie_ns.route('/')
+class MovieView(Resource):
+    def get(self):
+        director = request.args.get('director_id')
+        genre = request.args.get('genre_id')
+        year = request.args.get('year_id')
+
+        filter = {
+            'director_id': director,
+            'genre_id': genre,
+            'year_id': year
+        }
+
+        all_movies = movie_service.get_all(filter)
+        result = MovieSchema(many=True).dump(all_movies)
+        return result, 200
+
+    def post(self):
+        request_json = request.json
+        movie = movie_service.create(request_json)
+        return "", 201, {'location': f'/movies/{movie.id}'}
+
+
+@movie_ns.route('/<int:uid>')
+class MovieView(Resource):
+    def get(self, uid):
+        movie_object = movie_service.get_one(uid)
+        result = MovieSchema().dump(movie_object)
+        return result, 200
+
+    def put(self, uid):
+        request_json = request.json
+        if 'id' not in request_json:
+            request_json['id'] = uid
+            movie_service.update(request_json)
+            return "", 204
+
+    def delete(self, uid):
+        movie_service.delete(uid)
+        return "", 204
